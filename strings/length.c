7f710ff453ca8733c17983cf6602811a4633f48b
refath <mohammedrefath20@gmail.com>
2025-08-09 11:38:39 +0530

    Add length.c

---
+++ strings/length.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[50];
+    int len=0,i;
+    printf("Enter a string\n");
+    scanf("%s",str);
+    for(i=0;str[i]!='\0';i++){
+    len=strlen(str);
+    len++;
+    }
+    printf("The length of string is %d\n",len);
+}
