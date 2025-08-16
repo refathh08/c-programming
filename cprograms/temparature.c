bde6937ab121313886953aeacba811fefceeeb83
refathh08 <mohammedrefath20@gmail.com>
2025-08-16 12:06:13 +0530

    Add temparature.c

---
+++ cprograms/temparature.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int c,k;
+    printf("Enter the temparature in celcius\n");
+    scanf("%d",&c);
+    k=c+273;
+    printf("The temparature in kelvin is %d\n",k);
+    return 0;
+}
