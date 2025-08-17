fccb0969e417407932318a23988bdd285c0f253e
refathh08 <mohammedrefath20@gmail.com>
2025-08-17 20:42:38 +0530

    Add all

---
+++ cprograms/pattern2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j;
+	printf("Enter n value\n");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j+=2)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
