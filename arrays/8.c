56b6c434f69383e9c756fb479621660bf1d30019
refathh08 <mohammedrefath20@gmail.com>
2025-09-02 18:12:14 +0530

    8.c

--- arrays/8.c
+++ arrays/8.c
@@ -14,7 +14,7 @@ int main(){
     printf("The transposed matrix is\n");
     for(i=0;i<m;i++){
         for(j=0;j<n;j++){
-            printf("%d\t",a[j][i]);
+            printf("%d\n",a[j][i]);
         }
     }
     return 0;
