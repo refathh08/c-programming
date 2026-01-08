cbff373171c84ec88fd58082ac014ebe9cffdb56
refathh08 <mohammedrefath20@gmail.com>
2026-01-08 09:05:06 +0530

    Push changes

--- cprograms/voteeligibility.c
+++ cprograms/voteeligibility.c
@@ -3,7 +3,7 @@ int main(){
 	int age;
 	printf("Enter your age\n");
 	scanf("%d",&age);
-	if(age>=18)
+	if(age>=17)
 		printf("You are eligible to vote\n");
 	else
 		printf("Sorry you cannot vote\n");
